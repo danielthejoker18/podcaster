c75e18ee542612a1b78f248cff193a5d7f6e3bb7
Daniel Moreira <Danielmoreira@inforgeneses.com.br>
2025-05-29 12:42:34 -0300

    Remove pyttsx3 TTS provider and update TTS configuration

--- tts/__init__.py
+++ tts/__init__.py
@@ -1,6 +1,5 @@
 from .gtts_provider import synthesize as synthesize_gtts
 from .macos_provider import synthesize as synthesize_macos
-from .pyttsx3_provider import synthesize as synthesize_pyttsx3
 from .elevenlabs_provider import synthesize as synthesize_elevenlabs
 
 import os
@@ -10,8 +9,6 @@ def get_tts_provider(provider_name):
         return synthesize_gtts
     elif provider_name == "macos":
         return synthesize_macos
-    elif provider_name == "pyttsx3":
-        return synthesize_pyttsx3
     elif provider_name == "elevenlabs":
         return synthesize_elevenlabs
     else:
